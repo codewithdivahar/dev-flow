00edc3d0bcf06c1b294b0d9199e71d617e6914e7
Divahar Baskaran <codewithdivahar@gmail.com>
2024-11-13 01:52:01 -0500

    Added auth with next auth and github

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,28 @@
-export default function Home() {
-  return <div className=""></div>;
-}
+import React from "react";
+
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+const Home = async () => {
+  const session = await auth();
+
+  console.log("Session", session);
+  return (
+    <div>
+      <h1>Home</h1>
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">Log out</Button>
+      </form>
+    </div>
+  );
+};
+
+export default Home;
